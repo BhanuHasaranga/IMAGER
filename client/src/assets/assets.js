a97f815663eab1d30cae114f2c31f1e0190815cd
Bhanu Hasaranga <hasaranga20139@gmail.com>
2025-01-09 00:39:38 +0530

    add new icons

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -12,12 +12,13 @@ import profile_img_2 from './profile_img_2.png'
 import step_icon_1 from './step_icon_1.svg'
 import step_icon_2 from './step_icon_2.svg'
 import step_icon_3 from './step_icon_3.svg'
-import email_icon from './email_icon.svg'
-import lock_icon from './lock_icon.svg'
+import email_icon from './email_icon_2.svg'
+import lock_icon from './lock_icon_2.svg'
 import cross_icon from './cross_icon.svg'
 import star_group from './star_group.png'
 import credit_star from './credit_star.svg'
 import profile_icon from './profile_icon.png'
+import user_icon from './user_icon_2.svg'
 
 export const assets = {
     logo,
@@ -34,7 +35,8 @@ export const assets = {
     cross_icon,
     star_group,
     credit_star,
-    profile_icon
+    profile_icon,
+    user_icon
 }
 
 export const stepsData = [
